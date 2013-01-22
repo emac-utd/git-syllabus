b78ec0d60c7631f02dd534e647d10627201291ea
Max <max@teamatb.com>
2013-01-22 11:57:42 -0600

    Plugin metadata scaffolding.

---
+++ gitsyllabus.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: GitSyllabus
+Plugin URI: http://emac-utd.github.com/gitsyllabus
+Description: A tool to move syllabus data from WordPress to GitHub 
+Version: 0.1
+Author: EMAClab
+Author URI: http://github.com/emac-utd
+License: MIT
+*/
+?>
