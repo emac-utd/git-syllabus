7da8e2ab73cfe8e2eb202367be3dd5e01eb5ed59
Max <max@teamatb.com>
2013-01-29 14:00:14 -0600

    Building out OAuth

--- config.php
+++ config.php
@@ -1,22 +1,36 @@
 <?php 
-
+    
     function github_oauth_retrieval() {
         wp_redirect( 'https://github.com/login/oauth', $status = 302 );
     }
 
-
     class gitsyllabus_oauth {
-
-        $oauth_client_id = '';
-        $oauth_client_secret = '';
 
         const OAUTH_URL = 'https://github.com/login/oauth/';
 
-        function __construct() {
+        function __construct($client_id, $client_secret) {
             //create oauth object and store developer key
+            $this->$client_id = $client_id;
+            $this->$client_secret = $client_secret;            
         }
 
         function oauth_init() {
+            $args = array(
+                'client_id' => $client_id,
+                'state' => $state,
+                'status' => 302
+            );
+
+            $response = wp_remote_get( wp_redirect( ( OAUTH_URL . 'authorize' ), $args) ); 
+
+            if ( is_wp_error( $response ) ) {
+                echo 'oauth init effed up';
+            }
+
+            else {
+                //where to parse out code response?
+                // $this->$oauth_code = $response
+            }
 
         }
 
