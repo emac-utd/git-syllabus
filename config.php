64a42c2fe9aa7676156e9525e4e3b256a2136b07
Max <max@teamatb.com>
2013-01-30 16:35:44 -0600

    Building out API registration

--- config.php
+++ config.php
@@ -30,11 +30,31 @@
             else {
                 //where to parse out code response?
                 // $this->$oauth_code = $response
+
             }
 
         }
 
         function oauth_validate() {
+            $args = array(
+                'client_id' => $client_id,
+                'client_secret' => $client_secret,
+                'state' => $state,
+                'code' => $code
+            );
+
+            $response = wp_remote_post( OAUTH_URL . 'access_token', $args = array );
+
+
+            if ( is_wp_error( $response ) ) {
+                echo 'oauth validation effed up';
+            }
+
+            else {
+                //where to parse out code response?
+                // $this->$oauth_code = $response
+
+            }
 
         }
     }
