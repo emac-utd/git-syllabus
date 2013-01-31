3d5abd062da3822302da8866baf9d136b7b3d1b5
Max <max@teamatb.com>
2013-01-30 23:06:42 -0600

    Building out create_repo and commit functions.

--- github_api.php
+++ github_api.php
@@ -2,11 +2,11 @@
     add_action( 'publish_post', $github);
     class github_api {
 
-        const OAUTH_URL = 'https://github.com/login/oauth/';
+        const API_URL = 'https://api.github.com/';
         $has_repo = false;
 
         function __construct($request, $client_id, $state) { 
-            $this->$request = $request;
+            $this->$request = $requxest;
             $this->$client_id = $client_id;
             $this->$state = $state;
 
@@ -24,11 +24,35 @@
 
             //add conditional for creating for org? (different pathway)
             $args = array('name' => $name );
-            wp_remote_post( $OAUTH . '/user/repos', $args );
+            $response = wp_remote_post( API_URL . 'user/repos', $args );
+            if (is_wp_error( $response )) {
+                echo 'repo creation effed up';
+            }
+            else {
+                //right now just returning the url of the newly created repo.
+                return $response['body']['url'];
+            }
+
         }
 
-        function commit() {
-            
+        function commit($owner, $repo, $message, $author, $parents, $tree) {
+
+            $args = array(
+                'message' => $message,
+                'author' => $author,
+                'parents' => $parents,
+                'tree' => $tree, 
+            );
+                        
+            $response = wp_remote_post( API_URL . 'repos/' . $owner . '/' . $repo . '/git/commits' , $args = array )
+
+            if (is_wp_error( $response )) {
+                echo 'commit effed up';
+            }
+            else {
+                //right now just returning the url of the newly created repo.
+                return $response['body']['url'];
+            }    
         }
 
         function pull () {
@@ -36,7 +60,7 @@
         }
 
         function get_repo_status() {
-
+            //Need to figure out how deep to make this. Eventual thought is sync.
         }
     }
 
